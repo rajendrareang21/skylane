fa094e17e685c107eb8e4dbaf0cbd0042623d5b9
rajendrareang21 <150366324+rajendrareang21@users.noreply.github.com>
2023-12-01 21:55:02 +0000

    v4

--- controllers/productController.js
+++ controllers/productController.js
@@ -68,7 +68,7 @@ export const getProductController = async (req, res) => {
       .find({})
       .populate("category")
       .select("-photo")
-      .limit(12)
+      // .limit(12)
       .sort({ createdAt: -1 });
     res.status(200).send({
       success: true,
